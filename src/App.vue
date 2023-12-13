0ee8607bad2e243a4e9cae00cf02a7992933fa6d
MarcoPicca <il96marco@gmail.com>
2023-12-13 14:43:25 +0100

    app vite completata

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AppTitle from './components/AppTitle.vue'
+</script>
+
+<template>
+  
+  <AppTitle title="La mia prima app con Vite!" subTitle="se così si può definire" />
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+
+defineProps({
+  title: '',
+  subTitle: '',
+})
+
+
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+
+  <div class="card">
+    
+    <img src="../../public/vite.svg" alt="">
+  </div>
+
+  <h2>{{ subTitle }}</h2>
+  
+</template>
+
+<style scoped>
+
+</style>
